18d4f31bf5d04913198cbe5b044b37b544c010a7
ProgrammingIncluded <chencharlesh@gmail.com>
2024-12-07 03:06:55 -0800

    Add interactive mode and fix draw bug

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use rand::seq::SliceRandom;
 use rand::SeedableRng;
+use rand::Rng;
 use rand_pcg::Pcg32;
 use std::fmt;
 use std::io;
@@ -59,7 +60,7 @@ impl fmt::Display for Card {
                 write!(f, "*-{}", self.color)
             }
             CardType::Cancel => {
-                write!(f, "C-{}", self.color)
+                write!(f, "S-{}", self.color)
             }
             CardType::Reverse => {
                 write!(f, "R-{}", self.color)
@@ -76,6 +77,8 @@ struct Game {
     turn: usize,
     direction: bool,
     seed: u64,
+    chainable: bool,
+    accum: u32,
 }
 
 impl Game {
@@ -97,7 +100,7 @@ impl Game {
                 }
             }
         }
-
+  
         // Add the zeroes
         for color in [Color::RED, Color::BLUE, Color::GREEN, Color::YELLOW] {
             cards.push(Card {
@@ -106,7 +109,7 @@ impl Game {
                 variant: CardType::Value,
             })
         }
-
+  
         for _ in 0..2 {
             for color in [Color::RED, Color::BLUE, Color::GREEN, Color::YELLOW] {
                 cards.push(Card {
@@ -126,7 +129,7 @@ impl Game {
                 });
             }
         }
-
+  
         for _ in 0..4 {
             cards.push(Card {
                 value: 0,
@@ -163,6 +166,8 @@ impl Game {
             deck: cards,
             turn: 0,
             direction: false,
+            chainable: false,
+            accum: 0,
         };
     }
 }
@@ -189,7 +194,7 @@ impl fmt::Display for Move {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self.variant {
             Variant::Play => write!(f, "H{}{}", self.hand_idx, self.as_color),
-            Variant::Skip => write!(f, "S"),
+            Variant::Skip => write!(f, "S{}", self.as_color),
             Variant::DrawDeck => write!(f, "D"),
             Variant::Draw4 => write!(f, "D4->{}", self.as_color),
             Variant::Draw2 => write!(f, "D2"),
@@ -205,7 +210,7 @@ impl Game {
 
         let mut moves = vec![];
         for i in 0..hand.len() {
-            if let Some(m) = Game::playable(top, &hand[i], i, self.turn) {
+            if let Some(m) = Game::playable(top, &hand[i], i, self.turn, self.chainable) {
                 // Expand wild moves if available.
                 if m.as_color == Color::WILD {
                     for c in [Color::RED, Color::YELLOW, Color::BLUE, Color::GREEN] {
@@ -235,6 +240,8 @@ impl Game {
         let mut deck = self.deck.clone();
         let mut field = self.field.clone();
         let mut seed = self.seed;
+        let mut accum = self.accum;
+        let mut chainable = self.chainable;
 
         let direction = self.direction
             ^ if picked.variant == Variant::Reverse {
@@ -251,14 +258,35 @@ impl Game {
         let turn: i32 = ((self.turn as i32) + turn * sign).rem_euclid(self.players as i32);
         let turn = turn as usize;
 
-        if picked.variant != Variant::DrawDeck {
-            // slow copy and delete vs resized to perfection.
-            let mut removed_card = hands[picked.player_idx].remove(picked.hand_idx);
-            removed_card.color = picked.as_color.clone(); // Mainly for wild cards.
-            field.push(removed_card);
-        } else {
-            let removed_card = deck.remove(deck.len() - 1);
-            hands[picked.player_idx].push(removed_card);
+        match picked.variant {
+            Variant::DrawDeck => {
+                // Draw deck could resolve a chained draw, in which case, we have to draw more.
+                // Could just be normal draw, in which case, we just draw one.
+                if accum <= 0 {
+                    accum = 1
+                }
+                for _ in 0..accum {
+                    let removed_card = deck.remove(deck.len() - 1);
+                    hands[picked.player_idx].push(removed_card);
+                }
+
+                chainable = false;
+                accum = 0;
+            },
+            Variant::Draw2 | Variant::Draw4 => {
+                accum += if picked.variant == Variant::Draw2 { 2 } else { 4 };
+                chainable = true;
+                // slow copy and delete vs resized to perfection.
+                let mut removed_card = hands[picked.player_idx].remove(picked.hand_idx);
+                removed_card.color = picked.as_color.clone(); // Mainly for wild cards.
+                field.push(removed_card);
+            },
+            _ => {
+                // slow copy and delete vs resized to perfection.
+                let mut removed_card = hands[picked.player_idx].remove(picked.hand_idx);
+                removed_card.color = picked.as_color.clone(); // Mainly for wild cards.
+                field.push(removed_card);
+            }
         }
 
         // If there are no more cards, we need to reshuffle the deck from the field first.
@@ -278,8 +306,10 @@ impl Game {
             turn,
             field,
             seed,
-            players: self.players,
+            chainable,
             direction,
+            accum,
+            players: self.players,
         };
     }
 
@@ -288,9 +318,10 @@ impl Game {
         playing: &Card,
         hand_idx: usize,
         player_idx: usize,
+        chainable: bool,
     ) -> Option<Move> {
-        // Check if chainable.
-        if top_of_deck.variant != playing.variant
+        // Check if chainable. If not, then we can only play if variants match.
+        if top_of_deck.variant != playing.variant && chainable
             && (top_of_deck.variant == CardType::DrawTwo
                 || top_of_deck.variant == CardType::DrawFour)
         {
@@ -313,11 +344,9 @@ impl Game {
                 }
             }
             CardType::DrawTwo => {
-                // Chaining
                 move_variant = Variant::Draw2;
             }
             CardType::DrawFour => {
-                // Chaining
                 move_variant = Variant::Draw4;
             }
             CardType::Reverse => {
@@ -368,6 +397,9 @@ struct Args {
 
     #[arg(short, long, help = "Seed to play the game.", default_value_t = 0)]
     seed: u64,
+
+    #[arg(long, help = "Select a player to play as otherwise the game is simulated.", default_value_t = -1)]
+    play_as: i32,
 }
 
 fn main() {
@@ -391,7 +423,6 @@ fn main() {
     }
 
     let mut game = Game::init(args.players, num_decks, args.hand_size, args.seed);
-    let is_playable = true;
     loop {
         let moves = game.generate();
         let empty = game.hands.iter().enumerate().find(|&(_, m)| m.len() == 0);
@@ -403,6 +434,9 @@ fn main() {
         println!("Field Top: {}", game.field[game.field.len() - 1]);
         println!("Cards in Deck: {}", game.deck.len());
         println!("Cards in Field: {}", game.field.len());
+        if game.accum > 1 {
+            println!("Cards to Draw: {}", game.accum);
+        }
         println!();
 
         for h in 0..game.players {
@@ -424,7 +458,7 @@ fn main() {
         );
         println!();
 
-        let select_idx = if is_playable {
+        let select_idx = if args.play_as == game.turn as i32 {
             let mut final_value: usize = 0;
             loop {
                 println!("Input a valid move from 0 - {}", moves.len() - 1);
@@ -446,8 +480,10 @@ fn main() {
             }
             final_value
         } else {
-            // TODO: BEEP ROBOT HERE
-            0
+            // Random selection
+            let bot_selection = rand::thread_rng().gen_range(0..moves.len()) as usize;
+            println!("Bot has picked move: {}", bot_selection);
+            bot_selection
         };
         println!();
 
